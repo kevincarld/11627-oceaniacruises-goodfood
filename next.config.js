4a95bedfdac806414dc336da264c6ad51cf1a907
Kevin David <kevincarld@gmail.com>
2023-03-28 14:14:23 +1100

    Update meta

--- next.config.js
+++ next.config.js
@@ -17,19 +17,19 @@ const nextConfig = {
   },
   env: {
     appMeta: {
-      canonical: 'canonical in next.config.js', // https://brandedcontent.[masthead].com.au/xxx/xxx"
-      brand: 'brand in next.config.js', // e.g Black Hawk
-      title: 'title in next.config.js', // e.g Being a good pet parent starts at mealtime
-      description: 'description in next.config.js',
-      siteName: 'siteName Title in next.config.js', // The Sydney Morning Herald, Good Food
-      icon: 'smh', // smh, goodfood, nine, afr, traveller
-      themeColour: '#0a1633', // hex
-      ogImage: 'https://live-native-paid-content.pantheonsite.io/wp-content/uploads/2022/12/lecreuset-social-media-bg-1.jpg',
-      date: 'dd-mm-yy', // e.g 03-12-23
-      year: 'yyyy' // e.g 2023
+      canonical: 'https://brandedcontent.goodfood.com.au/oceaniacruises/board-world-best-culinary-cruise-line', // https://brandedcontent.[masthead].com.au/xxx/xxx"
+      brand: 'Oceania Cruises', // e.g Black Hawk
+      title: 'On board the world’s best culinary cruise line', // e.g Being a good pet parent starts at mealtime
+      description: 'Catering to only 650 to 1238 guests, the line’s small, luxurious ships such as Regatta, Marina and brand-new Vista feature stunning interiors, imaginative dining concepts, and the highest standards of residential-style luxury. ',
+      siteName: 'Good Food', // The Sydney Morning Herald, Good Food
+      icon: 'goodfood', // smh, goodfood, nine, afr, traveller
+      themeColour: '#C3A873', // hex
+      ogImage: 'tbd',
+      date: '01-04-23', // e.g 03-12-23
+      year: '2023' // e.g 2023
     },
-    jobCode: '11111-name', // used for nine ga scripts
-    guidelines: false, // shows or hides layout guideline bars
+    jobCode: '11627-oceaniacruises-goodfood', // used for nine ga scripts
+    guidelines: true, // shows or hides layout guideline bars
     activateMasthead: false, // shows or hides masthead
     mastheadOverride: false, // overrides masthead that's pulling from hostname: e.g honey,style,travel,kitchen,you,parenting,coach,pets,celebrity
   }
