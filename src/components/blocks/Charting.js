f7d27ed32ae76d83acc7c9d9a7e0707ba89f97d3
Kevin David <kevincarld@gmail.com>
2023-03-30 09:35:27 +1100

    Added more rough sections layout

--- src/components/blocks/Charting.js
+++ src/components/blocks/Charting.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Text, Button, Link, Image, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Text, Button, Link, Image, Grid, GridItem, Flex } from '@chakra-ui/react'
 import PreLine from 'components/util/PreLine'
 import { mock } from 'utils/common'
 import Container from '../util/Container'
@@ -10,16 +10,42 @@ import Figure from 'components/util/Figure'
 export default function Charting() {
 
   return (
-    <Box>
-      <h1>I am Charting</h1>
-    </Box>
+    <Grid gridTemplateColumns={{lg:'6fr 6fr',wide:'7fr 5fr'}} gap={{lg:'51px'}} pt={{lg:'93px'}} pb={{lg:'43px'}}>
+      <GridItem >
+        <Img dimension='1072x631' w={{base: 'full'}} fit='cover' mock='charting-1'/>
+        <Flex  direction='column' justify='center' align='center'  pt={{base:'32px',lg:'70px'}} pl={{base:'18px'}} 
+          pr={{base:'18px'}} pb={{base:'45px'}}>
+          <Flex align={{lg:'end'}} textAlign='center'direction={{base:'column' ,lg:'row'}}>
+            <Text {...texts.heading} fontSize={{base:'70px' ,lg:'136px'}}>Charting</Text>
+            <Text {...texts.heading} >A NEW COURSE</Text>
+          </Flex>
+          <Text {...texts.content} pt={{base:'100px'}} maxW='490px' >
+            Oceania Cruises introduces a new Allura-class vessel with its latest ship Vista, and reinforces the company’s reputation for culinary excellence with 11 exquisite dining options, three of which are brand-new, including innovative American restaurant Ember.
+          </Text>
+        </Flex>
+      </GridItem>
+      <GridItem mt={{lg:'142px'}}>
+        <Img dimension='793x866' w={{base: 'full'}} h='full' fit='cover' mock='charting-2'/>
+      </GridItem>
+    </Grid>
   )
 }
 
 // font styles
 const texts = {
-  xx: {
-
-  }
+  content: {
+    fontFamily: 'body',
+    fontWeight: 'normal',
+    color: 'black',
+    fontSize: {base: '17px'},
+    lineHeight: {base: '30px', lg: '32px'}
+  },
+  heading: {
+    fontFamily: 'heading',
+    fontWeight: 'normal',
+    color: 'black',
+    fontSize: {base: '37px', lg: '36px'},
+    lineHeight: {base: '44px', lg: '29px'}
+  },
 }
 
--- src/components/blocks/Notable.js
+++ src/components/blocks/Notable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Text, Button, Link, Image, Grid, GridItem } from '@chakra-ui/react'
+import { Box, Text, Button, Link, Image, Grid, GridItem, Flex } from '@chakra-ui/react'
 import PreLine from 'components/util/PreLine'
 import { mock } from 'utils/common'
 import Container from '../util/Container'
@@ -9,16 +9,32 @@ import Figure from 'components/util/Figure'
 export default function Notable() {
 
   return (
-    <Box>
-      <h1>I am Notable</h1>
-    </Box>
+    <Container>
+      <Flex gap={{lg:'53px'}} pt={{lg:'76px'}} direction={{base:'column-reverse', lg:'row'}}>
+        <Box ml={{d:'-182px'}}>
+          <Img dimension='780x473' w={{base: 'full'}} fit='cover' mock='notable'/>
+        </Box>
+        <Box pt={{base:'53px'}} pb={{base:'106px'}} maxW={{lg:'456px'}}
+                  pl={{base:'20px',lg:'0px'}} pr={{base:'15px',lg:'0px'}}>
+          <PreLine {...texts.content}>
+            {`Also notable is Vista’s variety of staterooms, including the most spacious standard staterooms at sea, and staterooms for solo travellers at Concierge Level. Vista also has eight bars and lounges and Aquamar Spa, for an entirely new era of wellness. Its 1200 guests are served by 800 crew members.
+            Vista makes its maiden voyage on a seven-day Founder’s Cruise between Rome and Venice on 13 May 2023, introduced earlier than planned due to exceptional demand. The journey is hosted by Oceania Cruises’ founder Frank J. Del Rio in celebration of the new ship and the brand’s 20th anniversary.`}
+          </PreLine>
+        </Box>
+      </Flex>
+    </Container>
   )
 }
 
 // font styles
 const texts = {
-  xx: {
-
-  }
+  content: {
+    fontFamily: 'body',
+    fontWeight: 'normal',
+    color: 'black',
+    fontSize: {base: '17px',},
+    lineHeight: {base: '30px', lg: '32px'}
+  },
+  
 }
 
